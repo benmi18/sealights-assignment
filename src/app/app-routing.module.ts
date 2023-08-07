4ad1ac03fe5c0444b61fe9a18299133a1def7fa1
Michaeli Ben <benmi@etoro.com>
2023-08-07 21:06:50 +0300

    create add-user page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AddUserComponent } from './views/add-user/add-user.component';
+
+const routes: Routes = [
+  { path: 'add-user', component: AddUserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/views/add-user/add-user.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  templateUrl: './add-user.component.html',
+  styleUrls: ['./add-user.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AddUserComponent {
+
+}
