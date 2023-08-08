2ad52e67ae63fb08b3a71f81f8c348901cef3513
Michaeli Ben <benmi@etoro.com>
2023-08-08 12:02:13 +0300

    clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AddUserComponent } from './views/add-user/add-user.component';
 import { UsersListComponent } from './views/users-list/users-list.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddUserComponent,
-    UsersListComponent
+    UsersListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
